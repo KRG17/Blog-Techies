f8ad883a396fb0a2fa0e517bf45d868ca897eb0f
KRG17 <kritankrishigoel@gmail.com>
2023-09-14 22:32:13 +0530

    commit

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
+const dns = require("dns");
+
+dns.setDefaultResultOrder("ipv4first");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images:{
-    domains:["lh3.googleusercontent.com","firebasestorage.googleapis.com"]
-  }
-}
+  images: {
+    domains: ["lh3.googleusercontent.com", "firebasestorage.googleapis.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
